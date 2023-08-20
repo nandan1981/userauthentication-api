8fca25b024752a470cd2c8a4505cf3e794e61c11
Nandan <Nandan1981@gmail.com>
2023-08-20 23:10:24 +0530

    Continuous monitoring changes and some refactoring

--- src/main/java/com/ecommerce/userauthentication/UserauthenticationApiApplication.java
+++ src/main/java/com/ecommerce/userauthentication/UserauthenticationApiApplication.java
@@ -7,21 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class UserauthenticationApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserauthenticationApiApplication.class, args);
 	}
 	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				 registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
-			}
-		};
-	}
+	/*
+	 * @Bean public WebMvcConfigurer corsConfigurer() { return new
+	 * WebMvcConfigurer() {
+	 * 
+	 * @Override public void addCorsMappings(CorsRegistry registry) {
+	 * registry.addMapping("/**").allowedOrigins("*").allowedMethods("*"); } }; }
+	 */
 
 }
---
+++ src/main/java/com/ecommerce/userauthentication/repository/UserPurchase.java
@@ -0,0 +1,5 @@
+package com.ecommerce.userauthentication.repository;
+
+public interface UserPurchase {
+
+}
