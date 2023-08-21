1b8f61e121b24ee013d5f75ed0d5174f8bad2f05
Nandan <Nandan1981@gmail.com>
2023-08-21 13:20:45 +0530

    jacoco plugin added

--- src/main/java/com/ecommerce/userauthentication/UserauthenticationApiApplication.java
+++ src/main/java/com/ecommerce/userauthentication/UserauthenticationApiApplication.java
@@ -13,13 +13,16 @@ public class UserauthenticationApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserauthenticationApiApplication.class, args);
 	}
-	
-	/*
-	 * @Bean public WebMvcConfigurer corsConfigurer() { return new
-	 * WebMvcConfigurer() {
-	 * 
-	 * @Override public void addCorsMappings(CorsRegistry registry) {
-	 * registry.addMapping("/**").allowedOrigins("*").allowedMethods("*"); } }; }
-	 */
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+			}
+		};
+	}
 
 }
